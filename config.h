630a973213294984d6bdceb06363dde2fe710721
user@Latitude-7420-void <user@Latitude-7420-void>
2026-06-25 22:48:58 -0400

    add sound controls to dwm

--- config.h
+++ config.h
@@ -61,9 +61,27 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[]  = { "brave", NULL };
+static const char *volupcmd[]    = { "wpctl", "set-volume", "-l", "1.0", "@DEFAULT_AUDIO_SINK@", "5%+", NULL };
+static const char *voldowncmd[]  = { "wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "5%-", NULL };
+static const char *volmutecmd[]  = { "wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL };
+static const char *micmutecmd[]  = { "wpctl", "set-mute", "@DEFAULT_AUDIO_SOURCE@", "toggle", NULL };
+static const char *playpausecmd[] = { "playerctl", "play-pause", NULL };
+static const char *nextcmd[]      = { "playerctl", "next", NULL };
+static const char *prevcmd[]      = { "playerctl", "previous", NULL };
+static const char *stopcmd[]      = { "playerctl", "stop", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
+	/* modifier    r                key        function        argument */
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+	{ 0,                            XF86XK_AudioMute,        spawn, {.v = volmutecmd } },
+	{ 0,                            XF86XK_AudioMicMute,     spawn, {.v = micmutecmd } },
+	{ 0,                            XF86XK_AudioPlay,        spawn, {.v = playpausecmd } },
+	{ 0,                            XF86XK_AudioPause,       spawn, {.v = playpausecmd } },
+	{ 0,                            XF86XK_AudioNext,        spawn, {.v = nextcmd } },
+	{ 0,                            XF86XK_AudioPrev,        spawn, {.v = prevcmd } },
+	{ 0,                            XF86XK_AudioStop,        spawn, {.v = stopcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd} },
