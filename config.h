e39092c2a6986a80bb568df98abb6b54feea1618
JI <ji@example.com>
2026-07-20 00:54:13 -0400

    fix st invoke for dwm so it always starts in $HOME

--- config.h
+++ config.h
@@ -62,10 +62,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "sh", "-c" ,"cd $HOME;exec st", NULL };
 static const char *keymapcmd[] = { "/bin/sh", "-c", "dwm-keymap", NULL };
 static const char *fmcmd[] = { "pcmanfm", NULL};
-static const char *floatcmd[] = { "st", NULL };
+static const char *floatcmd[] = { "sh", "-c" ,"cd $HOME;exec st", NULL };
 /* Negative x/y are offsets from the right/bottom edge of the selected monitor.
  * Set centered to 1 to ignore x/y and center on the selected monitor. */
 static const FloatingWindow floatwin = { floatcmd, -820, 20, 800, 500, 1 };
