8756af61fd781543b1dda934370737573ca85c2b
ji <user@dell-void>
2026-09-10 22:33:37 -0400

    add st-256color window class so swallow works

--- config.h
+++ config.h
@@ -34,6 +34,7 @@ static const Rule rules[] = {
 	{ "Gimp",                    NULL,    NULL,          0,         1,          0,           0,        -1 },
 	{ "StHelp",                  NULL,    NULL,          0,         1,          1,           1,        -1 },
 	{ "St",                      NULL,    NULL,          0,         0,          1,           0,        -1 },
+	{ "st-256color",             NULL,    NULL,          0,         0,          1,           0,        -1 },
 	{ "kitty",                   NULL,    NULL,          0,         0,          1,           0,        -1 },
 	{ "com.mitchellh.ghostty",   NULL,    NULL,          0,         0,          1,           0,        -1 },
 	{ NULL,                      NULL,    "Event Tester",0,         0,          0,           1,        -1 }, /* xev */
